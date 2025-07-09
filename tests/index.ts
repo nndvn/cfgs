9d61d3536bb0ff523d7672038251d6e36299ca92
NND <d@nnd.vn>
2025-07-09 12:04:45 +0700

    wip

--- tests/index.ts
+++ tests/index.ts
@@ -1,1 +1,20 @@
 console.log('Hello via Bun!')
+
+const items = [
+	{
+		description: 'Description of Item 1',
+		id: 1,
+		name: 'Item 1',
+		price: 10.99,
+		quantity: 5,
+	},
+	{
+		description: 'Description of Item 2',
+		id: 2,
+		name: 'Item 2',
+		price: 19.99,
+		quantity: 3,
+	},
+]
+
+console.log(items)
